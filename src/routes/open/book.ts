fa2d2139f736b95edad0a54923479b9d65cd0700
Jose Rodriguez <joser27@uw.edu>
2024-05-08 13:43:17 -0700

    init post commit

--- src/routes/open/book.ts
+++ src/routes/open/book.ts
@@ -1,11 +1,107 @@
-import express, {Request, Response, Router } from "express";
+import express, {NextFunction, Request, Response, Router } from "express";
 
 import {pool} from '../../core/db/sql_conn';
 
 const bookRouter: Router = express.Router();
 
+// TODO add this function to ../../core/utilities
+function mwValidNameBookBody(
+  request: Request,
+  response: Response,
+  next: NextFunction
+) {
+  //TODO add validity to body
+}
+const format = (resultRow) => //TODO fix formatting maybe?
+  `{${resultRow.book_id}}, [${resultRow.isbn13}], ${resultRow.authors}, ${resultRow.original_title}`;
+
+
+/**
+ * @api {post} /book Request to add an entry
+ * @apiName PostBook
+ * @apiGroup Books
+ *
+ * @apiParam {Number} book a unique book id.
+ * @apiParam {Number} isbn a books isbn number.
+ * @apiParam {String} authors a books author’s name.
+ * @apiParam {Number} publication year a books publication year.
+ * @apiParam {String} original_title a books original title.
+ * @apiParam {String} title a book title.
+ * @apiParam {String} image_url a books image url.
+ * @apiParam {String} image_small_url a books small image url.
+ *
+ * @apiSuccess (Success 201) {String} entry the string:
+ *       `{${resultRow.book_id}}, [${resultRow.isbn13}], ${resultRow.authors}, ${resultRow.original_title}`;
+ *
+ *  @apiError (400: Name exists) {String} message "Book exists"
+ *  @apiError (400: Missing Parameters) {String} message "Missing required information - please refer to documentation
+ */
+bookRouter.post(
+  '/',
+  mwValidNameBookBody,
+  (request: Request, response: Response, next: NextFunction) => {
+      const priority: string = request.body.priority as string;
+      // if (
+      //     validationFunctions.isNumberProvided(priority) &&
+      //     parseInt(priority) >= 1 &&
+      //     parseInt(priority) <= 3
+      // ) {
+      //     next();
+      // } else {
+      //     console.error('Invalid or missing Priority');
+      //     response.status(400).send({
+      //         message:
+      //             'Invalid or missing Priority - please refer to documentation',
+      //     });
+      // }
+  },
+  (request: Request, response: Response) => {
+      const theQuery = `
+      INSERT 
+      INTO
+      BOOKS(Book_id, Isbn13, Authors, Original_publication_year, Original_title, Title, Image_url, Small_image_url)
+      VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *`;
+      const values = [
+        request.body.book_id,
+        request.body.isbn13,
+        request.body.authors,
+        request.body.original_publication_year,
+        request.body.original_title ,
+        request.body.title,
+        request.body.image_url,
+        request.body.small_image_url,
+      ];
+
+      pool.query(theQuery, values)
+          .then((result) => {
+              response.status(201).send({
+                  entry: format(result.rows[0]),
+              });
+          })
+          .catch((error) => {
+              if (
+                  error.detail != undefined &&
+                  (error.detail as string).endsWith('already exists.') //TODO book exists
+              ) {
+                  console.error('Name exists');
+                  response.status(400).send({
+                      message: 'Name exists',
+                  });
+              } else {
+                  //log the error
+                  console.error('DB Query error on POST');
+                  console.error(error);
+                  response.status(500).send({
+                      message: 'server error - contact support',
+                  });
+              }
+          });
+  }
+);
+
+
 // Get book by ISBN
-bookRouter.get('ISBN/:isbn', async (request: Request, response: Response) => {
+bookRouter.get('/ISBN/:isbn', async (request: Request, response: Response) => {
   const theQuery = `
   SELECT 
   b.id,
