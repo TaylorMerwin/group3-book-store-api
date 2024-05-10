f7b41b4f90974d530c1a03b0c9a54bc1f65bd5d8
gstupart <gstupart@uw.edu>
2024-05-09 20:09:03 -0700

    Added ID route, Author route, fixed pagination on other get routes than all books

--- src/routes/open/book.ts
+++ src/routes/open/book.ts
@@ -100,7 +100,8 @@ bookRouter.get(
     '/year/:pubYear',
     async (request: Request, response: Response) => {
         const theQuery = `
-        SELECT 
+        SELECT * FROM
+        (SELECT 
         b.id,
         b.isbn13,
         b.authors,
@@ -118,9 +119,10 @@ bookRouter.get(
         SUM(CASE WHEN r.rating = 5 THEN 1 ELSE 0 END) as rating_5_star
       FROM books b
       LEFT JOIN ratings r ON b.id = r.book_id
-        WHERE b.publication_year = $3 AND b.id > ($2 * ($1 - 1))
+        WHERE b.publication_year = $3
         GROUP BY b.id, b.isbn13, b.authors, b.publication_year, b.original_title, b.title, b.image_url, b.image_small_url
-        ORDER BY b.id ASC
+        ORDER BY b.id ASC)
+        OFFSET $2 * ($1 - 1)
         LIMIT $2;`;
         const values = [
             request.query.page,
@@ -152,5 +154,109 @@ bookRouter.get(
         }
     }
 );
+
+// Get books by author name
+bookRouter.get(
+    '/author/:pattern',
+    async (request: Request, response: Response) => {
+        const theQuery = `
+    SELECT * FROM
+    (SELECT 
+    b.id,
+    b.isbn13,
+    b.authors,
+    b.publication_year,
+    b.original_title,
+    b.title,
+    b.image_url,
+    b.image_small_url,
+    COUNT(r.rating) as rating_count, 
+    AVG(r.rating) as average_rating,
+    SUM(CASE WHEN r.rating = 1 THEN 1 ELSE 0 END) as rating_1_star,
+    SUM(CASE WHEN r.rating = 2 THEN 1 ELSE 0 END) as rating_2_star,
+    SUM(CASE WHEN r.rating = 3 THEN 1 ELSE 0 END) as rating_3_star,
+    SUM(CASE WHEN r.rating = 4 THEN 1 ELSE 0 END) as rating_4_star,
+    SUM(CASE WHEN r.rating = 5 THEN 1 ELSE 0 END) as rating_5_star
+    FROM books b
+    LEFT JOIN ratings r ON b.id = r.book_id
+    WHERE b.authors LIKE $3
+    GROUP BY b.id, b.isbn13, b.authors, b.publication_year, b.original_title, b.title, b.image_url, b.image_small_url
+    ORDER BY b.id ASC)
+    OFFSET $2 * ($1 - 1)
+    LIMIT $2;`;
+        const values = [
+            request.query.page,
+            request.query.size,
+            '%' + request.params.pattern + '%',
+        ];
+        try {
+            const result = await pool.query(theQuery, values);
+            if (result.rowCount === 0) {
+                console.log(
+                    'Book not found with given text pattern:',
+                    request.params.pattern
+                );
+                response.status(404).send({
+                    message: 'No books with this text pattern in the database!',
+                });
+            } else {
+                response.send({
+                    entry: result.rows,
+                });
+            }
+        } catch (error) {
+            console.error('Error executing database query:', error);
+            response.status(500).send({
+                message: 'Error fetching book',
+            });
+        }
+    }
+);
+
+// Get book by ID
+bookRouter.get('/ID/:id', async (request: Request, response: Response) => {
+    const theQuery = `
+  SELECT 
+  b.id,
+  b.isbn13,
+  b.authors,
+  b.publication_year,
+  b.original_title,
+  b.title,
+  b.image_url,
+  b.image_small_url,
+  COUNT(r.rating) as rating_count, 
+  AVG(r.rating) as average_rating,
+  SUM(CASE WHEN r.rating = 1 THEN 1 ELSE 0 END) as rating_1_star,
+  SUM(CASE WHEN r.rating = 2 THEN 1 ELSE 0 END) as rating_2_star,
+  SUM(CASE WHEN r.rating = 3 THEN 1 ELSE 0 END) as rating_3_star,
+  SUM(CASE WHEN r.rating = 4 THEN 1 ELSE 0 END) as rating_4_star,
+  SUM(CASE WHEN r.rating = 5 THEN 1 ELSE 0 END) as rating_5_star
+FROM books b
+LEFT JOIN ratings r ON b.id = r.book_id
+WHERE b.id = $1
+GROUP BY b.id, b.isbn13, b.authors, b.publication_year, b.original_title, b.title, b.image_url, b.image_small_url
+ORDER BY b.id ASC;`;
+    const values = [request.params.id];
+
+    try {
+        const result = await pool.query(theQuery, values);
+        if (result.rowCount === 1) {
+            response.send({
+                entry: result.rows[0],
+            });
+        } else {
+            console.log('Book not found for ID:', request.params.id);
+            response.status(404).send({
+                message: 'Book not found',
+            });
+        }
+    } catch (error) {
+        console.error('Error executing database query:', error);
+        response.status(500).send({
+            message: 'Error fetching book' + error,
+        });
+    }
+});
 
 export { bookRouter };
